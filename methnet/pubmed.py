e6fc84584123884e9479ddeb14f497fabe2ca599
Kendra Oudyk <kendra.oudyk@gmail.com>
2020-05-26 22:26:05 -0400

    get full texts by looping through ids

--- methnet/pubmed.py
+++ methnet/pubmed.py
@@ -15,31 +15,44 @@ OUTPUT_DIR = pathlib.Path('results_pubmed')
 # esearch gets the IDs for papers matching the search results
 url_base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 url_unformatted = url_base + 'esearch.fcgi?db={db}&term={query}&usehistory=y'
-db = 'pubmed'
-query = 'music[mesh]+AND+functional+neuroimaging[mesh]+AND+fMRI+AND+2016[pdat]' \
-    + '+AND+pubmed+pmc+open+access[filter]'
+db = 'pmc'
+query = 'music+AND+AND+fMRI+AND+python+AND+2016[pdat]' \
+    + '+AND+open+access[filter]'
 esr_url = url_unformatted.format(db=db, query=query)
 print(esr_url, '\n')
 
-# get the esearch results
+# get the list of ID's from the esearch results
 esr_response = requests.get(esr_url)
 esr_root = ET.fromstring(esr_response.text)
-query_key = esr_root.find('QueryKey').text
-web_env = esr_root.find('WebEnv').text
+idlist_thing = esr_root.find('IdList')
+idlist = []
+for child in idlist_thing:
+    idlist.append(child.text)
 
-# assemble the esummary url
-esm_url_unformatted = url_base + 'esummary.fcgi?db={db}&query_key={query_key}&WebEnv={web_env}'
-esm_url = esm_url_unformatted.format(db=db, query_key=query_key, web_env=web_env)
-print(esm_url, '\n')
+# loop through the list of IDs to get the full texts
+for id in idlist:
+    ft_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc&id=' + id + '&retmode=xml'
+    print('\n', ft_urls)
 
-esm_response = requests.get(esm_url)
+# #
+# query_key = esr_root.find('QueryKey').text
+# web_env = esr_root.find('WebEnv').text
+#
+# # assemble the esummary url
+# esm_url_unformatted = url_base + 'esummary.fcgi?db={db}&query_key={query_key}&WebEnv={web_env}'
+# esm_url = esm_url_unformatted.format(db=db, query_key=query_key, web_env=web_env)
+# print(esm_url, '\n')
+#
+# esm_response = requests.get(esm_url)
+#
+# # assemble the efetch url
+# esf_url_unformatted = url_base + 'efetch.fcgi?db={db}&query_key={query_key}&WebEnv={web_env}' \
+# #+ '&retmode=xml'
+# esf_url = esf_url_unformatted.format(db=db, query_key=query_key, web_env=web_env)
+# print(esf_url, '\n')
 
-# assemble the efetch url
-esf_url_unformatted = url_base + 'efetch.fcgi?db={db}&query_key={query_key}&WebEnv={web_env}' \
-+ '&retmode=xml'
-esf_url = esf_url_unformatted.format(db=db, query_key=query_key, web_env=web_env)
-print(esf_url, '\n')
+# get
+# esf_response = requests.get(esf_url)
 
-esf_response = requests.get(esf_url)
 
 #IPython.embed()
