836d093710578457cb1ac3464c9a52efc471b399
Kendra Oudyk <kendra.oudyk@gmail.com>
2020-05-29 13:24:08 -0400

    save data, whether or not there's an error

--- methnet/pubmed.py
+++ methnet/pubmed.py
@@ -5,46 +5,57 @@ import requests
 from lxml import etree
 import IPython
 import pprint
+import json
 
 # user input
 field_query = 'fmri AND language'
-methods_queries = ['spm', 'afni', 'nilearn', 'fsl'] # THIS DOESN'T WORK
+methods_queries = ['spm', 'afni', 'nilearn', 'fsl']
 email = 'kendra.oudyk@mail.mcgill.ca'
 api_key_file = 'pubmed_api_key.txt'
 
-# make dictionaries to store results
-ids = {key: [] for key in methods_queries}
-urls = {key: [] for key in methods_queries}
+# make dictionary to store results
+data_fields = ['url', 'response', 'web_env', 'query_key', 'ids']
+level_2_data = {key: [] for key in data_fields}
+data = {key: level_2_data for key in methods_queries}
 
 # prepare url
 BASE_URL = ('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?')
 DATABASE = 'pubmed'
 TOOL = 'methnet'
-field_query = field_query.replace(' ', '+')
 api_key = open(api_key_file, 'r').read().strip()
 
+try:
+    for methods_query in methods_queries[0:1]:
+        url = (f'{BASE_URL}db={DATABASE}&term={field_query}' +\
+               f'+AND+{methods_query}&api_key={api_key}' +\
+               f'&usehistory=y&email={email}&tool={TOOL}')
+        url = url.replace('"', '%22')
+        url = url.replace(' ', '+')
 
-for methods_query in methods_queries:
-    url = (f'{BASE_URL}db={DATABASE}&term={field_query}' +\
-           f'+AND+{methods_query}&api_key={api_key}' +\
-           f'&usehistory=y&email={email}&tool={TOOL}')
-    urls[methods_query] = url
+        # get data returned from the search, and the query key and web env
+        # needed to re-use the search results from history
+        response = requests.get(url)
+        xml = response.content
+        tree = etree.XML(xml)
+        query_key = tree.find('QueryKey').text
+        web_env = tree.find('WebEnv').text
 
-    # get data returned from the search, and the query key and web env needed
-    # to re-use the search results from history
-    response = requests.get(url)
-    xml = response.content
-    tree = etree.XML(xml)
-    query_key = tree.find('QueryKey').text
-    web_env = tree.find('WebEnv').text
+        # populate data
+        data[methods_query]['url'] = url
+        data[methods_query]['response'] = response.text
+        data[methods_query]['web_env'] = web_env
+        data[methods_query]['query_key'] = query_key
 
-    # get ids of papers that include the given methods-related keyword
-    if int(tree.find('Count').text) > 0: # if there are any results
-        for id_element in tree.iter('Id'):
-            ids[methods_query].append(int(id_element.text))
+        # get ids of papers that include the given methods-related keyword
+        if int(tree.find('Count').text) > 0: # if there are any results
+            for id_element in tree.iter('Id'):
+                data[methods_query]['ids'].append(int(id_element.text))
 
-    if len(methods_queries) > 10:
-        time.sleep(.1) # pause so we don't exceed 10 requests per second
+        if len(methods_queries) > 10:
+            time.sleep(.1) # pause so we don't exceed 10 requests per second
+except:
+    with open('pubmed_data_dumped_by_error.json', 'w') as json_file:
+        json.dump(data, json_file)
 
-pprint.pprint(urls)
-pprint.pprint(ids)
+with open('pubmed_data.json', 'w') as json_file:
+    json.dump(data, json_file)
