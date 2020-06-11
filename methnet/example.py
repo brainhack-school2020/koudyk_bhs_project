9c6145e20a6d721f8faaabf3cfc776fe982789b0
Kendra Oudyk <kendra.oudyk@gmail.com>
2020-06-11 14:22:48 -0400

    specify title in example

--- methnet/example.py
+++ methnet/example.py
@@ -19,22 +19,24 @@
 import methnet_master as methnet
 from IPython.display import Image
 
-field_query = '"functional neuroimaging"[mesh] AND music[mesh]'
+field_query = ('"functional neuroimaging"[mesh] AND (fMRI[Title/Abstract] OR "functional magnetic resonance imaging"[Title/Abstract] OR "functional MRI"[Title/Abstract]) AND music[mesh]')
 list_methods_queries = ['spm', 'afni', 'nilearn', 'fsl']
 list_method_colors = ['green', 'dodgerblue', 'orange', 'red']
 data_id = "example"
 gif_id = "example"
+figure_title = "Music fMRI"
 
 gif_path, png_path, data = \
     methnet.get_methnet(field_query=field_query,
                    list_methods_queries=list_methods_queries,
                    data_id=data_id,
                    gif_id=gif_id,
-                   list_method_colors=list_method_colors, 
+                   list_method_colors=list_method_colors,
                    constant_color='black',
                    none_color='black',
-                   sort_by_year=True)
+                   sort_by_year=True,
+                   figure_title=figure_title)
 
-# if you're running this in a jupyter notebook, 
+# if you're running this in a jupyter notebook,
 # this line will show the gif inline
 Image(gif_path)
